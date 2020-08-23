3b0769e298c9f2f7eb68b8f6be2ecdb2538aa35a
Jacopo Cascioli <jacopo@strangemachines.io>
2020-08-24 00:01:15 +0200

    feat/q: add break_string/1

--- test/q_test.exs
+++ test/q_test.exs
@@ -1,4 +1,13 @@
 defmodule QTest do
   use ExUnit.Case
   doctest Q
+
+  test "break_string/1" do
+    assert Q.break_string("q:hello") == %{"q" => "hello"}
+  end
+
+  test "break_string/1 with more elements" do
+    result = Q.break_string("q:hello w:world")
+    assert result == %{"q" => "hello", "w" => "world"}
+  end
 end
