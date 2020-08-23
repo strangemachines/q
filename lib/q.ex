45765264623a97b525b9f6b354ef14b6a7e687f4
nomorepanic <nomorepanic@strangemachines.io>
2020-08-24 00:01:15 +0200

    feat/q: add break_string/1

--- lib/q.ex
+++ lib/q.ex
@@ -2,4 +2,18 @@ defmodule Q do
   @moduledoc """
   Documentation for `Q`.
   """
+
+  @doc """
+  Breaks the string in fragments and then puts the fragments into an
+  accumulator. For example, "x:0 y:1" would become %{"x" => 0, "y" => 1}
+  """
+  @spec break_string(string :: String.t()) :: map()
+  def break_string(string) do
+    string
+    |> String.split()
+    |> Enum.reduce(%{}, fn shard, acc ->
+      fragment = String.split(shard, ":")
+      Map.put(acc, List.first(fragment), List.last(fragment))
+    end)
+  end
 end
