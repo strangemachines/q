5369cb9a9d2d00f7a66b3bb02cd09605d194d016
Jacopo Cascioli <jacopo@strangemachines.io>
2020-09-12 23:49:59 +0200

    feat/q: add put_value/5

--- lib/q.ex
+++ lib/q.ex
@@ -14,6 +14,26 @@ defmodule Q do
     String.slice(value, String.length(operator)..-1)
   end
 
+  def put_value(nil, acc, _key, _value, :acc), do: acc
+
+  def put_value(nil, acc, key, value, :put), do: Map.put(acc, key, value)
+
+  @doc """
+  Puts the value in the accumulator, creating a value+operator map.
+  When there's no operator, the value can be put directly passing :put, or the
+  ignored with :acc, which returns the accumulator as is.
+  """
+  @spec put_value(
+          operator :: nil | String.t(),
+          acc :: map(),
+          key :: String.t(),
+          value :: String.t(),
+          mode :: :acc | :put
+        ) :: map()
+  def put_value(op, acc, key, value, _mode) do
+    Map.put(acc, key, %{value: Q.cut_operator(value, op), operator: op})
+  end
+
   @doc """
   Breaks the string in fragments and then puts the fragments into an
   accumulator. For example, "x:0 y:1" would become %{"x" => 0, "y" => 1}
