eceed4318553e37175810409e7dddcc371b5ccc7
Jacopo Cascioli <jacopo@strangemachines.io>
2020-09-13 15:47:08 +0200

    feat/q: add tests for param/4

---
+++ lib/search.ex
@@ -0,0 +1,10 @@
+defmodule Q.Search do
+  @moduledoc """
+  This module exists only so that I can test the macros in Q.
+  """
+  use Q
+
+  param("t", "type", ["-"], :put)
+
+  param("cmc", "cmc", [">", "<"], :acc)
+end
