965d04a75d70f42a15b46104e39b59402eaaa0f6
nomorepanic <nomorepanic@strangemachines.io>
2020-09-15 00:05:40 +0200

    chore/version: bump to 1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Q.MixProject do
   def project do
     [
       app: :q_parser,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       name: "Q Parser",
